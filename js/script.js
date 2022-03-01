8ae52c319ad52640f35c5123c60c99a292247434
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-02-28 21:21:54 -0600

    almost done

--- js/script.js
+++ js/script.js
@@ -10,7 +10,19 @@ let apiQuotes = [];
 function newQuote() {
   //pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-
+  authorText.textContent = quote.author;
+  // Check if author is blank and replace with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
   quoteText.textContent = quote.text;
 }
 
@@ -25,6 +37,16 @@ async function getQuotes() {
     //catch error here
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text= ${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+// twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
 getQuotes();
